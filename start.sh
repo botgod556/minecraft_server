3fae9d4bded062486a2376ee496b66ddeb081832
botgod556 <118589344+botgod556@users.noreply.github.com>
2024-05-19 15:07:58 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-cp -f https://raw.githubusercontent.com/botgod556/minecraft_server/main/server.properties server.properties
+rm server.properties
+wget https://raw.githubusercontent.com/botgod556/minecraft_server/main/server.properties
 java -Xmx28G -jar pufferfish-paperclip-1.20.4-R0.1-SNAPSHOT-reobf.jar
